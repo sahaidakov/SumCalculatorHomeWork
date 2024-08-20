5ef52197594065e3476518e2106ff5cd6aae083c
Artem Sahaidakov <sahaidakov@gmail.com>
2024-08-20 22:10:31 +0100

    checking whether int is higher than 65535

--- src/main/java/org/example/sumcalculator/CalculatorMain.java
+++ src/main/java/org/example/sumcalculator/CalculatorMain.java
@@ -4,7 +4,7 @@ public class CalculatorMain {
     public static void main(String[] args) {
         SumCalculator sumCalculator = new SumCalculator();
         try {
-            System.out.println(sumCalculator.sum(655359));
+            System.out.println(sumCalculator.sum(3));
         } catch (IllegalArgumentException e) {
             System.out.println("Illegal argument error");
         }
